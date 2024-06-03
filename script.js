9ea2ab0355ac8b4ef35d3c3d6765ab2552ab801d
Vicky Rai <165273117+Vickyrai28@users.noreply.github.com>
2024-06-03 17:36:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function red(){
+    document.body.style.backgroundColor = "red";
+}
+
+
+
+
+function blue(){
+    document.body.style.backgroundColor = "blue";
+}
+
+
+
+function green(){
+    document.body.style.backgroundColor = "green";
+}
+
+
+
+
+function yellow(){
+    document.body.style.backgroundColor = "yellow";
+}
+
+
+
+function aqua(){
+    document.body.style.backgroundColor = "aqua";
+}
+
+
+
+
+function violet(){
+    document.body.style.backgroundColor = "violet";
+}
+
+
+
+function orange(){
+    document.body.style.backgroundColor = "orange";
+}
+
+
+
+
+function grey(){
+    document.body.style.backgroundColor = "grey";
+}
+
+
+
+function purple(){
+    document.body.style.backgroundColor = "purple";
+}
+
+
+
+
+function pink(){
+    document.body.style.backgroundColor = "pink";
+}
+
+
+
+function brown(){
+    document.body.style.backgroundColor = "brown";
+}
+
+
+
+
+function black(){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "blue";
+}
